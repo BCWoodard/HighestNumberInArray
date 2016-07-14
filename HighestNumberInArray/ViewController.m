8367cd4375e0a19597fe0521d4ed4fb754ebc211
Brad Woodard <bcwoodard@gmail.com>
2016-07-14 15:29:30 -0500

    First commit

---
+++ HighestNumberInArray/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  HighestNumberInArray
+//
+//  Created by Brad Woodard on 7/14/16.
+//  Copyright © 2016 elementC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self findHighestNumberInArray];
+}
+
+- (void)findHighestNumberInArray {
+    NSArray *array = @[@17, @2, @8, @37, @24, @6];
+    NSInteger highestNumber = [array[0] integerValue];
+
+    for (int index = 0; index < array.count; index++) {
+        if ([array[index] integerValue] > highestNumber) {
+            highestNumber = [array[index] integerValue];
+        }
+    }
+    
+    NSLog(@"Highest Number: %li", highestNumber);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
